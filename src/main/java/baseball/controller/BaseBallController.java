adda23cd61ea864e20e51cf55b543403744c59e7
lcjltj <lcjltj@gmail.com>
2022-04-16 20:19:34 +0900

    feat : 기본 야구 게임 실행 구현
    프로그램 실행 시 기본 으로 실행 되는 야구 게임 기능 구현

---
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -0,0 +1,18 @@
+package baseball.controller;
+
+public class BaseBallController {
+
+    private static boolean play;
+
+    public static void start() {
+        play = true;
+
+        while (play) {
+        }
+    }
+
+    public static void requestRestart() {
+
+    }
+
+}
