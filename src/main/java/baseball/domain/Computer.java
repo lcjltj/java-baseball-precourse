988cc42881a5557970fd47661b449cff45698817
lcjltj <lcjltj@gmail.com>
2022-04-17 20:49:49 +0900

    feat : 입력 값과 랜덤 값 비교 기능 추가
    Randoms.pickUniqueNumbersInRange -> andoms.pickNumberInRange 으로 변경
    Set을 통한 중복이 없는 세자리 수 값 구현
    사용자 입력 값과 랜던값 비교 로직 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,26 +1,101 @@
 package baseball.domain;
 
+import baseball.view.InputView;
+import baseball.view.OutputView;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import static baseball.constant.Constants.*;
+import static baseball.constant.GameRule.*;
 
 public final class Computer {
 
-    private static final Computer INSTANCE = new Computer();
-    private List<Integer> numbers;
+    private final List<Integer> answer;
+    private int strike;
+    private int ball;
+    private boolean successful;
 
-    private Computer() {
+    public Computer() {
+        answer = generateRandomNumbers();
+        strike = 0;
+        ball = 0;
+        successful = false;
     }
 
-    public static Computer getInstance() {
-        INSTANCE.numbers = generateRandomNumber();
-        return INSTANCE;
+    public boolean playGame() {
+        while (!successful) {
+            init();
+            InputView.printNumberMessage();
+            List<Integer> inputNumber = Play.inputNumbers();
+            String message = compareNumber(inputNumber);
+            OutputView.printResult(message);
+        }
+
+        InputView.printRestartMessage();
+        return Play.isRestart();
     }
 
-    private static List<Integer> generateRandomNumber() {
-        return Randoms.pickUniqueNumbersInRange(MIN_VALUE, MAX_VALUE, LENGTH);
+    private void init() {
+        strike = 0;
+        ball = 0;
     }
 
+    private String compareNumber(final List<Integer> inputNumber) {
+        for (int i = 0; i < inputNumber.size(); i++) {
+            countStrike(inputNumber.get(i), i);
+            countBall(inputNumber.get(i), i);
+        }
+
+        isCorrect();
+        return outputMessage();
+    }
+
+    private void countStrike(final int num, final int index) {
+        if (answer.contains(num) && answer.indexOf(num) == index) {
+            strike++;
+        }
+    }
+
+    private void countBall(final int num, final int index) {
+        if (answer.contains(num) && answer.indexOf(num) != index) {
+            ball++;
+        }
+    }
+
+    private void isCorrect() {
+        successful = (strike == THREE_STRIKE);
+    }
+
+    private String outputMessage() {
+        if (strike > STRIKE_ZERO && ball == BALL_ZERO) {
+            return String.format(STRIKE_MESSAGE, strike);
+        }
+
+        if (strike == STRIKE_ZERO && ball > BALL_ZERO) {
+            return String.format(BALL_MESSAGE, ball);
+        }
+
+        if (strike > STRIKE_ZERO && ball > BALL_ZERO) {
+            return String.format(STRIKE_BALL_MESSAGE, ball, strike);
+        }
+
+        return OUT_MESSAGE;
+    }
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_VALUE, MAX_VALUE);
+    }
+
+    private List<Integer> generateRandomNumbers() {
+        Set<Integer> numberSet = new HashSet<>();
+
+        while (numberSet.size() < LENGTH) {
+            numberSet.add(generateRandomNumber());
+        }
+
+        return new ArrayList<>(numberSet);
+    }
 }
